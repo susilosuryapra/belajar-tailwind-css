b340252ae245f8051fb228bb551c09915fd4aaf4
susilosuryapra <susilosuryap@gmail.com>
2024-09-08 12:38:43 +0700

    commit video kedua

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,16 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        roboto: ["roboto"],
+        inter: ["inter"],
       },
       colors: {
         orchid: "#741AAC",
         cinnabar: "#E43D40",
+        coral: "#FF8370",
+        ungu: "#5D50C6",
+        pink: "#F85E9F",
+        orange: "#FF5722",
+        grey: "#191825",
       },
     },
   },
@@ -17,3 +22,4 @@ module.exports = {
 
 // apapun yang ada di input.css akan langsung diterapkan ke html selayaknya file css pada umumnya
 // sedangkan apapun yang ada di file tailwind.config.js hanya diterapkan apabila kita memanggilnya dalam class
+// ternyata font yang sudah dipanggil dengan input.css tidak usah dipanggil lagi di tailwind.config.js
