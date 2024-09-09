12549f0d7f96fa7cdaa36d3fa3bfa3d8b8821802
susilosuryapra <susilosuryap@gmail.com>
2024-09-09 22:25:44 +0700

    commit video ketiga

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,22 @@ module.exports = {
         orange: "#FF5722",
         grey: "#191825",
       },
+      dropShadow: {
+        "sm-shadow": [
+          "0px 548px 219px rgba(0, 0, 0, 0.01)",
+          "0px 308px 185px rgba(0, 0, 0, 0.04)",
+          "0px 137px 137px rgba(0, 0, 0, 0.06)",
+          "0px 34px 75px rgba(0, 0, 0, 0.07)",
+          "0px 0px 0px rgba(0, 0, 0, 0.07)",
+        ],
+        "lg-shadow": [
+          "0px 81px 32px rgba(0, 0, 0, 0.01)",
+          "0px 45px 27px rgba(0, 0, 0, 0.05)",
+          "0px 20px 20px rgba(0, 0, 0, 0.09)",
+          "0px 5px 11px rgba(0, 0, 0, 0.1)",
+          "0px 0px 0px rgba(0, 0, 0, 0.1)",
+        ],
+      },
     },
   },
   plugins: [],
